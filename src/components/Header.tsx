949ee6466654ebfaaf1518f26b9524ace1589e67
Arubesu <bruno.arubesu@gmail.com>
2022-12-18 17:24:08 -0300

    chore: add sidebar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import igniteLogo from '../assets/ignite-logo.svg';
 export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-          <img src={igniteLogo} alt="Logotipo do Ignite" />    </header>
+          <img src={igniteLogo} alt="Green logo with a slight transparency composed of 2 connected triangles" />    
+    </header>
   );
 }
