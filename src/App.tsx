cf9d2b1c8f5a8070c29d8180e7d7fdf04e7793b9
Arubesu <bruno.arubesu@gmail.com>
2022-12-18 16:44:50 -0300

    chore: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { Header } from './components/Header'
+
+import './global.css'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Header />
     </div>
   )
 }
